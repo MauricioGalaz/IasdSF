ec31b64964b84d1aa9cddf4d4c8d363f33cdbf17
MauricioGalaz <mgalazestay@hotmail.com>
2026-01-27 18:21:21 -0300

    Actualizacion v2 forzando cache

--- sw.js
+++ sw.js
@@ -1,8 +1,25 @@
-const CACHE_NAME = 'iasd-limache-v1';
+const CACHE_NAME = 'iasd-limache-v2'; // <--- Cambia el número cada vez
 const urlsToCache = ['./', './index.html'];
 
 self.addEventListener('install', e => {
+    // Esto obliga al nuevo Service Worker a tomar el control de inmediato
+    self.skipWaiting(); 
     e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
+});
+
+self.addEventListener('activate', e => {
+    // Esto borra la versión vieja del celular
+    e.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
 });
 
 self.addEventListener('fetch', e => {
